176d764570d56ccabcd78188d3ccfc5c5130eb38
Lars Willighagen <lars.willighagen@gmail.com>
2018-12-04 17:54:21 +0100

    Add solution for Day 4

--- day-4/solution.js
+++ day-4/solution.js
@@ -3,6 +3,13 @@ function parseLog (log) {
   return [new Date(date), msg]
 }
 
+const log = $0
+  .textContent
+  .split('\n')
+  .slice(0, -1)
+  .map(parseLog)
+  .sort(([a], [b]) => a - b)
+
 function groupByShift (grouped, element, i) {
   if (element[1].match(/Guard #(\d+) begins shift/)) {
     grouped.push([element])
@@ -12,44 +19,57 @@ function groupByShift (grouped, element, i) {
   return grouped
 }
 
-function combineLogs (logs) {
-  const [[shift, guard], sleep, wake, ...extra] = logs
+function combineShift ([[shift, guard], ...sleeps]) {
   const [id] = guard.match(/Guard #(\d+) begins shift/).slice(1)
-  if (sleep) {
-    const [totalSleep, totalWake] = extra.reduce(([sleep, wake], [time], i) => {
-      return i % 2 === 0
-        ? [sleep + time, wake]
-        : [sleep, wake + time]
-    }, [sleep, wake])
-    return {id, shift, sleep: (totalWake - totalSleep) / 60000}
-  } else {
-    return {id, shift, sleep: 0}
-  }
+  const toggles = sleeps.map(([time]) => time.getMinutes())
+
+  return [id, toggles]
 }
 
-function totalSleep (guards, guard) {
-  if (!guards[guard.id]) {
-    guards[guard.id] = 0
+function groupSleepByGuard (guards, [id, toggles]) {
+  if (!guards[id]) {
+    guards[id] = Array(60).fill(0)
   }
 
-  guards[guard.id] += guard.sleep
-
+  let awake = true
+  for (let min = 0; min < 60; min++) {
+    if (min === toggles[0]) {
+      awake = !awake
+      toggles.shift()
+    }
+    if (!awake) {
+      guards[id][min]++
+    }
+  }
+  
   return guards
 }
 
-const log = $0
-  .textContent
-  .split('\n')
-  .slice(0, -1)
-  .map(parseLog)
-  .sort(([a], [b]) => a - b)
+let sleep = log
   .reduce(groupByShift, [])
-  .map(combineLogs)
+  .map(combineShift)
+  .reduce(groupSleepByGuard, {})
 
-const sleepiest = Object
-  .entries(log.reduce(totalSleep, {}))
-  .reduce(([maxSleep, sleepiest], [guard, sleep]) => {
-    return sleep > maxSleep
-      ? [sleep, guard]
-      : [maxSleep, sleepiest]
-  }, [-Infinity])
+let sleepiestGuard = Object.entries(sleep).reduce(([maxSleep, sleepiest], [guard, sleep]) => {
+  let totalSleep = sleep.reduce((total, min) => total + min)
+  return totalSleep > maxSleep
+    ? [totalSleep, guard]
+    : [maxSleep, sleepiest]
+}, [-Infinity])[1]
+
+let sleepiestMinute = Object.entries(sleep[sleepiestGuard]).reduce(([maxSleep, sleepiest], [minute, sleep]) => {
+  return sleep > maxSleep
+    ? [sleep, minute]
+    : [maxSleep, sleepiest]
+}, [-Infinity])[1]
+
+console.log('star 1:', sleepiestGuard * sleepiestMinute)
+
+let [regularSleeper, regularMinute] = Object.entries(sleep).reduce(([maxFreq, ...rest], [guard, sleep]) => {
+  let freq = Math.max(...sleep)
+  return freq > maxFreq
+    ? [freq, guard, sleep.indexOf(freq)]
+    : [maxFreq,  ...rest]
+}, [-Infinity]).slice(1)
+
+console.log('star 2:', regularSleeper * regularMinute)
