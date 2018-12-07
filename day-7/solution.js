4ee11842befe0368d2c7f392969a967d86e79591
Lars Willighagen <lars.willighagen@gmail.com>
2018-12-07 11:03:58 +0100

    Update solution.js

--- day-7/solution.js
+++ day-7/solution.js
@@ -1,22 +1,22 @@
-const notFirst = new Set()
 const all = new Set()
+const notFirst = new Set()
 
-const rules = $0.textContent
+const [rules, reverseRules] = $0.textContent
   .trim()
   .split('\n')
-  .reduce((rules, rule) => {
+  .reduce(([rules, reverseRules], rule) => {
     let [a, b] = rule
-      .match(//)
-      .slice(-1)
-    if (!rules[a]) {
-      rules[a] = []
-    }
+      .match(/Step (.) must be finished before step (.) can begin\./)
+      .slice(1)
+    if (!rules[a]) { rules[a] = [] }
     rules[a].push(b)
+    if (!reverseRules[b]) { reverseRules[b] = [] }
+    reverseRules[b].push(a)
     notFirst.add(b)
     all.add(a)
     all.add(b)
-    return rules
-  }, {})
+    return [rules, reverseRules]
+  }, [{}, {}])
 
 function difference (a, b) {
   let difference = new Set(a)
@@ -30,9 +30,24 @@ let order = []
 let next = Array.from(difference(all, notFirst))
 
 while (next.length) {
-  let rule = next.sort().shift()
+  let rule
+  while (!rule) {
+    rule = next.shift()
+    if (reverseRules[rule] &&  !reverseRules[rule].every(step => order.includes(step))) {
+      next.push(rule)
+      rule = null
+    }
+  }
+
+  if (order.includes(rule)) {
+    continue
+  }
+
   order.push(rule)
-  next.push(...rules[rule])
+  next.push(...(rules[rule] || []))
+  next.sort()
 }
 
 console.log('star 1:', order.join(''))
+
+
